59fdc723c82d6c68799fba1cb8a672a43e639cb8
AustinBH <harlowaustin2@gmail.com>
2019-05-19 20:33:57 -0700

    All basic functions working as intended

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Cult.new("Big Iron", "Agua Fria", 1905, "The stranger there among them had a big iron on his hip")
+c2 = Cult.new("Lil Copper", "Agua Fria", 1945, "K, bud")
+
+f1 = Follower.new("Curtis", 21, "Get rich or die tryin'")
+f2 = Follower.new("John", 27, "Go")
+
+c1.recruit_follower(f1)
+c1.recruit_follower(f2)
+
+f1.join_cult(c2)
+
+puts BloodOath.all.each {|oath|puts oath.initiation_date}
+
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
